53e1953aade350786299261be1e8ab85b1a753e8
Shannon Bertucci <shannon.bertucci@gmail.com>
2013-08-21 16:13:26 -0400

    Pascals Trapezoid

---
+++ 147.clj
@@ -0,0 +1,34 @@
+;; Anything you type in here will be executed
+;; immediately with the results shown on the
+;; right.
+
+(defn nextrow [row]
+  (vec
+   (concat
+    (take 1 row)
+    (map
+     #(apply + %)
+     (partition 2 1 row))
+     (take-last 1 row))))
+
+(nextrow [3 1 2])
+
+
+(defn pascals [row]
+  (iterate (fn nextrow [x]
+  (vec (concat
+          (take 1 x)
+          (map
+           #(apply +' %)
+           (partition 2 1 x))
+          (take-last 1 x))))
+           row))
+
+(rest (take 101 (pascals [2 4 2])))
+
+(defn pasctrap [v]
+  (let [v1 (concat [0] v)
+        v2 (concat v [0])]
+    (lazy-seq (cons v (pasctrap (map +' v1 v2))))))
+
+(pasctrap [1])
